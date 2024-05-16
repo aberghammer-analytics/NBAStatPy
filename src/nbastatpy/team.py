fa33de09611a52fc96989b74a9abc13b0ce404ca
Anthony Berghammer <aberghammer.analytics@gmail.com>
2024-05-16 19:50:43 -0400

    Added team

---
+++ src/nbastatpy/team.py
@@ -0,0 +1,181 @@
+from datetime import date
+from io import BytesIO
+from typing import List
+
+import nba_api.stats.endpoints as nba
+import pandas as pd
+import requests
+from loguru import logger
+from nba_api.stats.endpoints import leaguegamefinder
+from nba_api.stats.static import players, teams
+from PIL import Image
+
+from utils import Formatter
+
+
+class Team:
+    def __init__(self, team_abbreviation: str, season_year: str = None, playoffs=False):
+        if season_year:
+            self.season_year = season_year
+        else:
+            self.season_year = Formatter.get_current_season_year()
+
+        self.info = teams.find_team_by_abbreviation(team_abbreviation)
+
+        self.season = Formatter.format_season(self.season_year)
+        self.season_type = "Regular Season"
+        if playoffs:
+            self.season_type = "Playoffs"
+
+        for attr_name, value in self.info.items():
+            setattr(self, attr_name.lower(), self.info.get(attr_name, None))
+
+    def get_roster(self) -> List[pd.DataFrame]:
+        self.roster = nba.CommonTeamRoster(
+            self.id, season=self.season
+        ).get_data_frames()
+        return self.roster
+
+    def get_leaders(self) -> pd.DataFrame:
+        self.leaders = nba.FranchiseLeaders(team_id=self.id).get_data_frames()[0]
+        return self.leaders
+
+    def get_franchise_players(self) -> pd.DataFrame:
+        self.franchise_players = nba.FranchisePlayers(
+            team_id=self.id
+        ).get_data_frames()[0]
+        return self.franchise_players
+
+    def get_season_lineups(self) -> pd.DataFrame:
+        self.season_lineups = nba.LeagueDashLineups(
+            team_id_nullable=self.id,
+            season=self.season,
+            season_type_all_star=self.season_type,
+        ).get_data_frames()[0]
+        self.season_lineups["season"] = self.season
+        self.season_lineups["season_type"] = self.season_type
+
+        return self.season_lineups
+
+    def get_opponent_shooting(self) -> pd.DataFrame:
+        self.opponent_shooting = nba.LeagueDashOppPtShot(
+            team_id_nullable=self.id,
+            season=self.season,
+            season_type_all_star=self.season_type,
+        ).get_data_frames()[0]
+        self.opponent_shooting["season"] = self.season
+        self.opponent_shooting["season_type"] = self.season_type
+
+        return self.opponent_shooting
+
+    def get_player_clutch(self) -> pd.DataFrame:
+        self.player_clutch = nba.LeagueDashPlayerClutch(
+            team_id_nullable=self.id,
+            season=self.season,
+            season_type_all_star=self.season_type,
+        ).get_data_frames()[0]
+        self.player_clutch["season"] = self.season
+        self.player_clutch["season_type"] = self.season_type
+
+        return self.player_clutch
+
+    def get_player_shots(self) -> pd.DataFrame:
+        self.player_shots = nba.LeagueDashPlayerPtShot(
+            team_id_nullable=self.id,
+            season=self.season,
+            season_type_all_star=self.season_type,
+        ).get_data_frames()[0]
+        self.player_shots["season"] = self.season
+        self.player_shots["season_type"] = self.season_type
+
+        return self.player_shots
+
+    def get_player_shot_locations(self) -> pd.DataFrame:
+        self.player_shot_locations = nba.LeagueDashPlayerShotLocations(
+            team_id_nullable=self.id,
+            season=self.season,
+            season_type_all_star=self.season_type,
+        ).get_data_frames()[0]
+        self.player_shot_locations["season"] = self.season
+        self.player_shot_locations["season_type"] = self.season_type
+
+        return self.player_shot_locations
+
+    def get_player_stats(self) -> pd.DataFrame:
+        self.player_stats = nba.LeagueDashPlayerStats(
+            team_id_nullable=self.id,
+            season=self.season,
+            season_type_all_star=self.season_type,
+        ).get_data_frames()[0]
+        self.player_stats["season"] = self.season
+        self.player_stats["season_type"] = self.season_type
+
+        return self.player_stats
+
+    def get_player_point_defend(self) -> pd.DataFrame:
+        self.player_point_defend = nba.LeagueDashPtDefend(
+            team_id_nullable=self.id,
+            season=self.season,
+            season_type_all_star=self.season_type,
+        ).get_data_frames()[0]
+        self.player_point_defend["season"] = self.season
+        self.player_point_defend["season_type"] = self.season_type
+
+        return self.player_point_defend
+
+    def get_player_hustle(self) -> pd.DataFrame:
+        self.player_hustle = nba.LeagueHustleStatsPlayer(
+            team_id_nullable=self.id,
+            season=self.season,
+            season_type_all_star=self.season_type,
+        ).get_data_frames()[0]
+        self.player_hustle["season"] = self.season
+        self.player_hustle["season_type"] = self.season_type
+
+        return self.player_hustle
+
+    def get_lineup_details(self) -> pd.DataFrame:
+        self.lineup_details = nba.LeagueLineupViz(
+            team_id_nullable=self.id,
+            season=self.season,
+            season_type_all_star=self.season_type,
+            minutes_min=1,
+        ).get_data_frames()[0]
+        self.lineup_details["season"] = self.season
+        self.lineup_details["season_type"] = self.season_type
+
+        return self.lineup_details
+
+    def get_player_on_details(self) -> pd.DataFrame:
+        self.player_on_details = nba.LeaguePlayerOnDetails(
+            team_id=self.id, season=self.season, season_type_all_star=self.season_type
+        ).get_data_frames()[0]
+        self.player_on_details["season"] = self.season
+        self.player_on_details["season_type"] = self.season_type
+
+        return self.player_on_details
+
+    def get_player_matchups(self, defense=False) -> pd.DataFrame:
+        if defense:
+            self.player_matchups = nba.LeagueSeasonMatchups(
+                def_team_id_nullable=self.id,
+                season=self.season,
+                season_type_playoffs=self.season_type,
+            ).get_data_frames()[0]
+        else:
+            self.player_matchups = nba.LeagueSeasonMatchups(
+                off_team_id_nullable=self.id,
+                season=self.season,
+                season_type_playoffs=self.season_type,
+            ).get_data_frames()[0]
+
+        self.player_matchups["season"] = self.season
+        self.player_matchups["season_type"] = self.season_type
+
+        return self.player_matchups
+
+
+if __name__ == "__main__":
+    team_name = "MIL"
+    team = Team(team_name)
+    print(team.get_player_matchups())
--- src/nbastatpy/utils.py
+++ src/nbastatpy/utils.py
@@ -1,7 +1,16 @@
+from datetime import datetime
+
 import pandas as pd
 
 
 class Formatter:
+
+    def get_current_season_year() -> str:
+        current_datetime = datetime.now()
+        current_season_year = current_datetime.year
+        if current_datetime.month <= 9:
+            current_season_year -= 1
+        return current_season_year
 
     def format_season(season_year: int) -> str:
         return "{}-{}".format(int(season_year), str(int(season_year) + 1)[2:])
