6c276bb52732ed97f4e42b9238d26ca5ba81d929
Anthony Berghammer <aberghammer.analytics@gmail.com>
2024-12-12 21:57:26 -0500

    update tests

--- tests/test_season.py
+++ tests/test_season.py
@@ -6,15 +6,3 @@ SEASON_YEAR = "2020"
 def test_season_creation():
     season = Season(SEASON_YEAR)
     assert season.season_year == SEASON_YEAR
-
-
-def test_lineups():
-    season = Season(SEASON_YEAR)
-    lineups = season.get_lineups()
-    assert lineups.shape[0] > 0
-
-
-def test_salaries():
-    season = Season(SEASON_YEAR)
-    salaries = season.get_salaries()
-    assert salaries.shape[0] > 0
--- tests/test_team.py
+++ tests/test_team.py
@@ -6,21 +6,3 @@ TEAM_NAME = "MIL"
 def test_team_creation():
     team = Team(TEAM_NAME)
     assert team.info.get("abbreviation") == TEAM_NAME
-
-
-def test_team_roster():
-    team = Team(TEAM_NAME)
-    team.get_roster()
-    assert team.roster is not None
-
-
-def test_team_logo():
-    team = Team(TEAM_NAME)
-    team.get_logo()
-    assert team.logo is not None
-
-
-def test_team_salary():
-    team = Team(TEAM_NAME, season_year="2020")
-    team.get_salary()
-    assert team.salary_df is not None
